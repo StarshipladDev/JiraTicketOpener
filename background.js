845861b3953d61d271ab413f681a96baba74355c
StarshipladDev <StarshipladDev@gmail.com>
2020-08-17 21:40:56 +1200

    Final Changes

--- background.js
+++ background.js
@@ -2,7 +2,7 @@
 chrome.declarativeContent.onPageChanged.removeRules(undefined, function () {
     chrome.declarativeContent.onPageChanged.addRules([{
         conditions: [new chrome.declarativeContent.PageStateMatcher({
-            pageUrl: { hostEquals: 'jira' },
+            pageUrl: { urlPrefix: 'https://jira' },
         })
         ],
         actions: [new chrome.declarativeContent.ShowPageAction()]
